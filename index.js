e926f7f3473a3be52fc146d3e1515597edf34a0b
Aniketmane29 <aniket.mane2997@gmail.com>
2025-06-25 21:17:56 +0530

    Updated Code

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello from Docker container.......");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Docker container');
+});
+
+// IMPORTANT: bind to 0.0.0.0, not localhost
+app.listen(7000, '0.0.0.0', () => {
+  console.log('Server running on port 7000');
+});
+
